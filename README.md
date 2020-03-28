c0cc9047348e5d6e5d120724486c6f23b8160af8
Pablo Ferreiro <pferreiromero@gmail.com>
2020-03-29 00:09:46 +0100

    Recordar sesión, nuevos iconos y configuración

--- README.md
+++ README.md
@@ -2,6 +2,14 @@
 
 Esta es una implementación open-source distribuida bajo la licencia GPLv3 del programa de seguimiento escolar PASEN.
 
-Utiliza Python 3, Glade y las librerías requests y GTK.
+Utiliza Python 3, Glade, y las librerías requests, configparser y GTK.
 
 Actualmente testeado usando una cuenta de PASEN de alumno.
+
+# TODO
+
+Queda por implementar las siguientes funciones:
+
+https://www.juntadeandalucia.es/educacion/seneca/seneca/jsp/pasendroid/getComunicaciones (Comunicaciones profe <-> alumno) | GET?
+https://www.juntadeandalucia.es/educacion/seneca/seneca/jsp/pasendroid/getConvocatorias (Actividades evaluables) | POST?
+https://www.juntadeandalucia.es/educacion/seneca/seneca/jsp/pasendroid/getNotas (NOTAS) | POST
--- openpasen.py
+++ openpasen.py
@@ -1,11 +1,11 @@
 import requests
 import json
+import configparser
 import gi
-#import configparser TODO
 gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk
 
-#Initial variables
+# Initial variables
 headers = {
     "Content-Type": "application/json; charset=UTF-8"
 }
@@ -20,6 +20,8 @@ class userinfo:
         self.nombre = main.json()['RESULTADO'][0]['USUARIO']
         self.matricula = main.json()['RESULTADO'][0]['MATRICULAS'][0]['X_MATRICULA']
         self.centro = main.json()['RESULTADO'][0]['MATRICULAS'][0]['X_CENTRO']
+        self.curso = main.json()['RESULTADO'][0]['MATRICULAS'][0]['CURSO']
+        self.unidad = main.json()['RESULTADO'][0]['MATRICULAS'][0]['UNIDAD']
     
 #Facilitar las requests
 def req(method, url):
@@ -30,25 +32,16 @@ def req(method, url):
         data = requests.post(url, headers=headers, cookies=jar)
         return data
 
-#GTK Handler
+# GTK Handler
 class Handler:
+    # Global (Destroy)
     def onDestroy(self, *args):
        Gtk.main_quit()
     
-    #Ventana error al iniciar sesión
-    def on_error_volver_boton_clicked(self, button):
-        builder.get_object("login_error").destroy()
-    
-    #Ventana acerca de activada
-    def on_about_button_activate(self, button):
-        builder = Gtk.Builder()
-        builder.get_object("about").show()
-    
-    #Botón login pulsado
+    # --------- Botones --------- #
+    # Botón login pulsado
     def on_login_clicked(self, button):
-        username = builder.get_object('username').get_text()
-        password = builder.get_object('password').get_text()
-        body = 'p={ "version":"11.9.1" }&USUARIO=' + username + '&CLAVE=' + password
+        body = 'p={"version":"11.9.1"}&USUARIO=' + builder.get_object('username').get_text() + '&CLAVE=' + builder.get_object('password').get_text()
         login = requests.post("https://www.juntadeandalucia.es/educacion/seneca/seneca/jsp/pasendroid/login", headers=headerslogin, data=body)
         # Comprobando si se ha iniciado sesión correctamente
         if (login.text != '{"ESTADO":{"CODIGO":"C"}}'):
@@ -59,25 +52,68 @@ class Handler:
         jar.set('SenecaP', login.cookies['SenecaP'], domain='www.juntadeandalucia.es', path='/')
         jar.set('JSESSIONID', login.cookies['JSESSIONID'], domain='www.juntadeandalucia.es', path='/')
         print("Las cookies usadas son: " + str(jar))
+        if (builder.get_object('sesioniniciada').get_active):
+            config['Login'] = {
+                'SenecaP': login.cookies['SenecaP'],
+                'JSESSIONID': login.cookies['JSESSIONID']}
+            with open('config.ini', 'w') as configfile:
+                config.write(configfile)
         builder.get_object("login_menu").hide()
         builder.get_object("main_menu").show()
     
-    #Ejecutar cuando el menú principal se muestre
+    # Ventana error al iniciar sesión
+    def on_error_volver_boton_clicked(self, button):
+        builder.get_object("login_error").destroy()
+    
+    #Main menu
+    
+    # Botón avisos pulsado
+    def on_avisos_boton_clicked(self, button):
+        print("EN CONSTRUCCIÓN")
+        #builder.get_object("avisos").show()
+        #avisos = req("GET", "https://www.juntadeandalucia.es/educacion/seneca/seneca/jsp/pasendroid/avisos")
+        #builder.get_object("avisos_label").set_markup(avisos.json()['RESULTADO'][0]['D_AVISO']) #Error, target
+    
+    #Barra de herramientas
+
+    # Botón acerca de pulsado
+    def on_about_boton_activate(self, button):
+        builder.get_object("about").show()
+    
+    def on_perfil_boton_activate(self, button):
+        print("WIP")
+    # Botón cerrar sesión pulsado
+    def on_logout_boton_activate(self,button):
+        config.remove_section('Login')
+        with open("config.ini", "w") as configfile:
+            config.write(configfile)
+        builder.get_object('main_menu').hide()
+        builder.get_object('login_menu').show()
+    
+    # --------- Funciones que se ejecutan cuando se abre una ventana --------- #
+    # Ejecutar cuando el menú principal se muestre
     def on_main_menu_show(self, *args):
         main = req("GET", "https://www.juntadeandalucia.es/educacion/seneca/seneca/jsp/pasendroid/infoSesion")
         print("Escribiendo datos del usuario...")
+        global user
         user = userinfo(main)
         builder.get_object("bienvenido_buffer").set_text("Bienvenido, " + user.nombre)
-
         # TODO Hacer aparecer imagen alumno
 
+# Iniciar builder y mostrar menú login
+config = configparser.ConfigParser()
+config.read('config.ini')
 builder = Gtk.Builder()
 builder.add_from_file("assets/glade/openpasen.glade")
 builder.connect_signals(Handler())
-builder.get_object("login_menu").show()
-Gtk.main()
+# Si hay una configuración ya creada iniciar sesión con esa información
+try:
+    config.items('Login')
+    jar = requests.cookies.RequestsCookieJar()
+    jar.set('SenecaP', config['Login']['senecap'], domain='www.juntadeandalucia.es', path='/')
+    jar.set('JSESSIONID', config['Login']['JSESSIONID'], domain='www.juntadeandalucia.es', path='/')
+    builder.get_object("main_menu").show()
+except configparser.NoSectionError:
+    builder.get_object("login_menu").show()
 
-# TODO https://www.juntadeandalucia.es/educacion/seneca/seneca/jsp/pasendroid/avisos (Avisos ventana emergente) | GET
-# TODO https://www.juntadeandalucia.es/educacion/seneca/seneca/jsp/pasendroid/getComunicaciones (Comunicaciones profe <-> alumno) | GET
-# TODO https://www.juntadeandalucia.es/educacion/seneca/seneca/jsp/pasendroid/getConvocatorias (Actividades evaluables) | POST?
-# TODO https://www.juntadeandalucia.es/educacion/seneca/seneca/jsp/pasendroid/getNotas (NOTAS) | POST
+Gtk.main()
