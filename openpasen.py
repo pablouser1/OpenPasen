c44528e38fdde75cbe6b867302eb4db71ebfcfdf
Pablo Ferreiro <pferreiromero@gmail.com>
2020-04-02 22:01:47 +0200

    Cambios en interfaz, solución de bugs

--- openpasen.py
+++ openpasen.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 import requests
 import json
 import configparser
@@ -18,10 +20,13 @@ class userinfo:
     # Al conseguir iniciar sesión, establecer variables, se utilizan luego
     def __init__(self, main):
         self.nombre = main.json()['RESULTADO'][0]['USUARIO']
-        self.matricula = main.json()['RESULTADO'][0]['MATRICULAS'][0]['X_MATRICULA']
-        self.centro = main.json()['RESULTADO'][0]['MATRICULAS'][0]['X_CENTRO']
         self.curso = main.json()['RESULTADO'][0]['MATRICULAS'][0]['CURSO']
         self.unidad = main.json()['RESULTADO'][0]['MATRICULAS'][0]['UNIDAD']
+        self.denominacion = main.json()['RESULTADO'][0]['MATRICULAS'][0]['DENOMINACION']
+        self.tutor = main.json()['RESULTADO'][0]['MATRICULAS'][0]['TUTOR']
+        #IDs que se usarán después
+        self.matricula = main.json()['RESULTADO'][0]['MATRICULAS'][0]['X_MATRICULA']
+        self.centro = main.json()['RESULTADO'][0]['MATRICULAS'][0]['X_CENTRO']
     
 #Facilitar las requests
 def req(method, url):
@@ -32,11 +37,16 @@ def req(method, url):
         data = requests.post(url, headers=headers, cookies=jar)
         return data
 
+
 # GTK Handler
 class Handler:
-    # Global (Destroy)
+    # Global
     def onDestroy(self, *args):
        Gtk.main_quit()
+    
+    def delete_event(self, widget, event):
+        widget.hide()
+        return True 
     
     # --------- Botones --------- #
     # Botón login pulsado
@@ -45,19 +55,33 @@ class Handler:
         login = requests.post("https://www.juntadeandalucia.es/educacion/seneca/seneca/jsp/pasendroid/login", headers=headerslogin, data=body)
         # Comprobando si se ha iniciado sesión correctamente
         if (login.text != '{"ESTADO":{"CODIGO":"C"}}'):
-            print("Error al iniciar sesión, ¿ha escrito la contraseña correcta?")
+            builder.get_object('login_error').show()
             quit(1)
         global jar
         jar = requests.cookies.RequestsCookieJar()
         jar.set('SenecaP', login.cookies['SenecaP'], domain='www.juntadeandalucia.es', path='/')
         jar.set('JSESSIONID', login.cookies['JSESSIONID'], domain='www.juntadeandalucia.es', path='/')
         print("Las cookies usadas son: " + str(jar))
-        if (builder.get_object('sesioniniciada').get_active):
-            config['Login'] = {
+
+        # Guardar configuración de inicio de sesión si se ha marcado el botón de mantener sesión iniciada
+        if (str(builder.get_object('sesioniniciada').get_active()) == "True"):
+            print("Guardando información de inicio de sesión...")
+            config['Cookies'] = {
                 'SenecaP': login.cookies['SenecaP'],
                 'JSESSIONID': login.cookies['JSESSIONID']}
-            with open('config.ini', 'w') as configfile:
-                config.write(configfile)
+            
+            config['Login'] = {
+                'Username': builder.get_object('username').get_text(),
+                'Password': builder.get_object('password').get_text()}
+            
+            config['Config'] = {
+                'LoginRemember': "Y"}
+        else:
+            config['Config'] = {
+                'LoginRemember': "N"}
+        
+        with open('config.ini', 'w') as configfile:
+            config.write(configfile)
         builder.get_object("login_menu").hide()
         builder.get_object("main_menu").show()
     
@@ -74,16 +98,34 @@ class Handler:
         #avisos = req("GET", "https://www.juntadeandalucia.es/educacion/seneca/seneca/jsp/pasendroid/avisos")
         #builder.get_object("avisos_label").set_markup(avisos.json()['RESULTADO'][0]['D_AVISO']) #Error, target
     
+    def on_notas_clicked(self,button):
+        builder.get_object("notas_menu").show()
+
+    def on_continuar_notas_boton_clicked(self, button):
+        builder.get_object("notascombo").get_active_text()
     #Barra de herramientas
 
     # Botón acerca de pulsado
-    def on_about_boton_activate(self, button):
+    def on_ayuda_clicked(self, button):
         builder.get_object("about").show()
     
-    def on_perfil_boton_activate(self, button):
-        print("WIP")
+    def on_perfil_activate(self, button):
+        builder.get_object("perfil_menu").show()
+
+        builder.get_object("curso_label").set_markup(user.curso)
+        builder.get_object("unidad_label").set_markup(user.unidad)
+        builder.get_object("centro_label").set_markup(user.denominacion)
+        builder.get_object("tutor_label").set_markup(user.tutor)
+
+    def on_centro_activate(self, button):
+        builder.get_object("centro_menu").show()
+        body = "X_CENTRO=" + user.centro
+        centro = requests.post("https://www.juntadeandalucia.es/educacion/seneca/seneca/jsp/pasendroid/datosCentro", headers=headerslogin, cookies=jar, data=body)
+        centro.json()['RESULTADO'][0]['DATOS']
+    
     # Botón cerrar sesión pulsado
-    def on_logout_boton_activate(self,button):
+    def on_cerrarsesion_activate(self,button):
+        config.remove_section('Cookies')
         config.remove_section('Login')
         with open("config.ini", "w") as configfile:
             config.write(configfile)
@@ -93,11 +135,29 @@ class Handler:
     # --------- Funciones que se ejecutan cuando se abre una ventana --------- #
     # Ejecutar cuando el menú principal se muestre
     def on_main_menu_show(self, *args):
-        main = req("GET", "https://www.juntadeandalucia.es/educacion/seneca/seneca/jsp/pasendroid/infoSesion")
-        print("Escribiendo datos del usuario...")
-        global user
-        user = userinfo(main)
-        builder.get_object("bienvenido_buffer").set_text("Bienvenido, " + user.nombre)
+        try:
+            main = req("GET", "https://www.juntadeandalucia.es/educacion/seneca/seneca/jsp/pasendroid/infoSesion")
+            print("Escribiendo datos del usuario...")
+            global user
+            user = userinfo(main)
+            builder.get_object("bienvenido_label").set_text("Bienvenido, " + user.nombre)
+        
+        #Si hay un KeyError es que las cookies han caducado, por lo que intenta volver a iniciar sesión
+        except KeyError:
+            builder.get_object('main_menu').hide()
+            print("Las cookies han expirado, generando nuevas...")
+            body = 'p={"version":"11.9.1"}&USUARIO=' + config['Login']['Username'] + '&CLAVE=' + config['Login']['Password']
+            login = requests.post("https://www.juntadeandalucia.es/educacion/seneca/seneca/jsp/pasendroid/login", headers=headerslogin, data=body)
+            if (login.text != '{"ESTADO":{"CODIGO":"C"}}'):
+                print("Error al iniciar sesión, ¿tienes conexión a Internet?")
+                quit(1)
+            if (config['Config']['LoginRemember'] == "Y"):
+                config['Cookies'] = {
+                    'SenecaP': login.cookies['SenecaP'],
+                    'JSESSIONID': login.cookies['JSESSIONID']}
+                with open('config.ini', 'w') as configfile:
+                    config.write(configfile)
+                    builder.get_object('main_menu').show()
 
 # Iniciar builder y mostrar menú login
 config = configparser.ConfigParser()
@@ -105,12 +165,13 @@ config.read('config.ini')
 builder = Gtk.Builder()
 builder.add_from_file("assets/glade/openpasen.glade")
 builder.connect_signals(Handler())
+
 # Si hay una configuración ya creada iniciar sesión con esa información
 try:
     config.items('Login')
     jar = requests.cookies.RequestsCookieJar()
-    jar.set('SenecaP', config['Login']['senecap'], domain='www.juntadeandalucia.es', path='/')
-    jar.set('JSESSIONID', config['Login']['JSESSIONID'], domain='www.juntadeandalucia.es', path='/')
+    jar.set('SenecaP', config['Cookies']['senecap'], domain='www.juntadeandalucia.es', path='/')
+    jar.set('JSESSIONID', config['Cookies']['JSESSIONID'], domain='www.juntadeandalucia.es', path='/')
     builder.get_object("main_menu").show()
 except configparser.NoSectionError:
     builder.get_object("login_menu").show()
